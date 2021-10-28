3bffd885fa6bc0af6c13a943d494353003417072
lopcarcar <carlos.lop.car95@gmail.com>
2021-10-28 22:39:41 +0200

    Pantallas Login y Registro casi terminadas

--- app/src/main/java/com/example/lopezcarreiracarlosproyectopmdm/LoginActivity.kt
+++ app/src/main/java/com/example/lopezcarreiracarlosproyectopmdm/LoginActivity.kt
@@ -3,24 +3,24 @@ package com.example.lopezcarreiracarlosproyectopmdm
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.lopezcarreiracarlosproyectopmdm.databinding.ActivityMainBinding
+import com.example.lopezcarreiracarlosproyectopmdm.databinding.ActivityLoginBinding
 
 
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btAcceder.setOnClickListener{
+        binding.btLoginAcceder.setOnClickListener{
 
         }
 
-        binding.tvRegistro.setOnClickListener{
+        binding.tvLoginRegistro.setOnClickListener{
 
             //Iniciamos RegistroUsuarios al hacer click en Registro
             val intent = Intent(this,RegistroUsuariosActivity::class.java)
--- app/src/main/java/com/example/lopezcarreiracarlosproyectopmdm/RegistroUsuariosActivity.kt
+++ app/src/main/java/com/example/lopezcarreiracarlosproyectopmdm/RegistroUsuariosActivity.kt
@@ -1,11 +1,27 @@
 package com.example.lopezcarreiracarlosproyectopmdm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lopezcarreiracarlosproyectopmdm.databinding.ActivityRegistroUsuariosBinding
 
 class RegistroUsuariosActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityRegistroUsuariosBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registro_usuarios)
+        binding = ActivityRegistroUsuariosBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btRegistrar.setOnClickListener{
+
+            //SharedPreferences
+
+            //Volver a Login
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
